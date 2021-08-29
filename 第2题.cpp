9a9a78392fb2d5c0acc08fe161cc524e27d0de04
ljjhh <88570271+ljjhh@users.noreply.github.com>
2021-08-29 18:41:17 +0800

    Create 第2题.cpp
    给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target  ，写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
    
    解题：标准的二分法

---
+++ 第2题.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n, head,tail,mid;
+        n=nums.size()-1;
+        for(head=0,tail=n;head<=tail;){
+            mid=(head+tail)/2;
+            if(target<nums[mid]){
+                tail=mid-1;
+            }
+            else if(target>nums[mid]){
+                head=mid+1;
+            }
+            else return mid;
+        }
+        return -1;
+    }
+};
