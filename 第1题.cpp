a5e6234c2936c5b8d50d3a869c397b8ee5d234f1
ljjhh <88570271+ljjhh@users.noreply.github.com>
2021-08-29 18:33:57 +0800

    Update 第1题.cpp
    有个马戏团正在设计叠罗汉的表演节目，一个人要站在另一人的肩膀上。出于实际和美观的考虑，在上面的人要比下面的人矮一点且轻一点。已知马戏团每个人的身高和体重，请编写代码计算叠罗汉最多能叠几个人。
    先排序：高度升序排列，相同高度的宽度降序排列
    然后DP数组下标 i 位置记录长为 i+1 最长递增序列末尾数字最小值
    最后返回DP数组长度

--- 第1题.cpp
+++ 第1题.cpp
@@ -4,10 +4,10 @@ public:
         vector<pair<int,int>> tmp;
         for(int i = 0; i < height.size(); i++) tmp.push_back({height[i], weight[i]});
         sort(tmp.begin(), tmp.end(), [](const pair<int,int> &a, const pair<int,int> &b) {
+
             return a.first == b.first ? a.second > b.second : a.first < b.first;
         });
-        vector<int> dp; //长度为N的地方 最小的数字
-        for(const auto &[h, w]: tmp) {
+        vector<int> dp; //长度为N的地方 最小的数字        for(const auto &[h, w]: tmp) {
             auto p = lower_bound(dp.begin(), dp.end(), w);  //二分查找第一个大于等于的地方
             if(p == dp.end()) dp.push_back(w);
             else *p = w;
